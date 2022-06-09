1bef9cc2adaf4f0fff958dcc27b3fcc66aa3aad9
LuanTedesco <luanfelipe2040@gmail.com>
2022-06-09 20:30:58 -0300

    Atualização 09/06

---
+++ Modulo_1/4 - Exercicios_30.04/Ruby/exercicio1.rb
@@ -0,0 +1,8 @@
+puts("Digite a senha: ")
+senha = gets.to_i
+
+if senha == 1234
+  puts("Senha correta!")
+else
+  puts("Senha incorreta!")
+end
---
+++ Modulo_1/4 - Exercicios_30.04/Ruby/exercicio4.rb
@@ -0,0 +1,30 @@
+puts("Digite o numero correspondente ao mês: ")
+num = gets.to_i
+
+if num == 1
+  puts("Janeiro")
+elsif num == 2
+  puts("Fevereiro")
+elsif num == 3
+  puts("Março")
+elsif num == 4
+  puts("Abril")
+elsif num == 5
+  puts("Maio")
+elsif num == 6
+  puts("Junho")
+elsif num == 7
+  puts("Junho")
+elsif num == 8
+  puts("Agosto")
+elsif num == 9
+  puts("Setembro")
+elsif num == 10
+  puts("Outubro")
+elsif num == 11
+  puts("Novembro")
+elsif num == 12
+  puts("Dezembro")
+else
+  puts("Não existe mês com este número!")
+end

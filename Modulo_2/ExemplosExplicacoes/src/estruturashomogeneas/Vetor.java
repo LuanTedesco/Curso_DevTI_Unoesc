a908d3befabf473a5da00c2914ff2ba65a8d660a
LuanTedesco <luanfelipe2040@gmail.com>
2022-06-11 18:54:47 -0300

    Atualização 11/06 Tarde

--- Modulo_2/ExemplosExplicacoes/src/estruturashomogeneas/Vetor.java
+++ Modulo_2/ExemplosExplicacoes/src/estruturashomogeneas/Vetor.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class Vetor {
 
   public static void main(String[] args) throws Exception {
-    Scanner ler = new Scanner(System.in);
-    int[] nota = new int[5];
-    int contador;
+    try (Scanner ler = new Scanner(System.in)) {
+      int[] nota = new int[5];
+      int contador;
 
-    for (contador = 0; contador <= 4; contador++) {
-      System.out.println("Digite a nota " + (contador + 1));
-      nota[contador] = ler.nextInt();
-    }
+      for (contador = 0; contador <= 4; contador++) {
+        System.out.println("Digite a nota " + (contador + 1));
+        nota[contador] = ler.nextInt();
+      }
 
-    for (contador = 0; contador <= 4; contador++) {
-      System.out.println(nota[contador]);
+      for (contador = 0; contador <= 4; contador++) {
+        System.out.println(nota[contador]);
+      }
+      ler.close();
     }
-    ler.close();
   }
 }
---
+++ Modulo_2/Exercicios/src/exercicios1106/Atividade3.java
@@ -0,0 +1,26 @@
+package Modulo_2.Exercicios.src.exercicios1106;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Atividade3 {
+
+  public static void main(String[] args) {
+    try (Scanner scan = new Scanner(System.in)) {
+      ArrayList<Integer> numeros = new ArrayList<>();
+      int contador;
+
+      for (contador = 1; contador <= 10; contador++) {
+        System.out.print("Digite um n�mero: ");
+        numeros.add(scan.nextInt());
+      }
+      System.out.println(
+        "O maior valor da lista �: " + Collections.max(numeros)
+      );
+      System.out.println(
+        "O menor valor da lista �: " + Collections.min(numeros)
+      );
+    }
+  }
+}

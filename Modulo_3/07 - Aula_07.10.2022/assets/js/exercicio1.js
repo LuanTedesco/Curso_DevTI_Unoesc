3198c23703fad27cb17f2988201a1f7d377f713f
LuanTedesco <luanfelipe2040@gmail.com>
2022-10-07 21:33:11 -0300

    Atualização aula 10/07/2022

---
+++ Modulo_3/07 - Aula_07.10.2022/assets/js/exercicio1.js
@@ -0,0 +1,8 @@
+let num1 = 2
+let num2 = 1
+
+if (num1 > num2) {
+  console.log(`O numero ${num1} é maior que o numero ${num2}`)
+} else if (num1 < num2) {
+  console.log(`O numero ${num2} é maior que o numero ${num1}`)
+}
---
+++ Modulo_3/07 - Aula_07.10.2022/assets/js/inputs.js
@@ -0,0 +1,105 @@
+// alert(oi)
+
+// console.log('Hello world')
+
+//let email = prompt('Digite seu email,')
+//alert('O email' + email + 'será usado pra span. :) é mentira')
+
+// -------------------------------------------------------------------------
+
+// let notaStr = prompt('Qual a sua nota?', '')
+// let trabStr = prompt('Qual o valor do trabalho?', '')
+// let nota = parseFloat(notaStr, 10) // parseInt
+// let trab = parseFloat(trabStr, 10)
+// nota = nota + trab
+// alert('Sua nota é: ' + nota)
+
+// -------------------------------------------------------------------------
+
+// let vegetariano = confirm('Você é vegetariano?')
+// if (vegetariano == true) {
+//   alert('Coma mais proteínas vegetais')
+// } else {
+//   alert('Coma menos gordura')
+// }
+
+// -------------------------------------------------------------------------
+
+// const idade = 33
+// const peso = 77
+
+// const altura = 1.65
+
+// let indiceMassaCorporal = peso / altura ** 2
+
+// console.log(`O IMC é ${indiceMassaCorporal.toFixed(2)}`)
+
+// -------------------------------------------------------------------------
+
+// const idade = 20
+// if (idade >= 16 && idade < 18) {
+//   console.log('voto facultativo')
+// }
+// if (idade < 16) {
+//   console.log('Não pode votar')
+// }
+// if (idade > 76) {
+//   console.log('não obrigado votar')
+// } else {
+//   console.log('Obrigado a votar')
+// }
+
+// -------------------------------------------------------------------------
+
+// let semaforo = 'verde'
+
+// if (semaforo == 'verde') {
+//   console.log('Pode passar')
+// } else if (semaforo == 'amarelo') {
+//   console.log('Atenção')
+// } else if (semaforo == 'vermelho') {
+//   console.log('Pare')
+// }
+
+// -------------------------------------------------------------------------
+
+// let username = 'username123'
+// let password = '123456'
+
+// if (username == 'username123' && password == '123456') {
+//   console.log('Usuário Authenticado')
+// } else {
+//   console.log('Login ou senha inválidos')
+// }
+
+// -------------------------------------------------------------------------
+
+// const count = 10
+
+// if (count === 0) {
+//   console.log('Não tem estudantes')
+// } else if (count === 1) {
+//   console.log('Tem somente um estudante')
+// } else {
+//   console.log(`estamos em ${count} estudantes`)
+// }
+
+// -------------------------------------------------------------------------
+
+// let condition = false
+
+// if (condition == true) {
+//   console.log('A condição é verdadeira')
+// } else {
+//   console.log('A condição é falsa')
+// }
+
+// -------------------------------------------------------------------------
+
+// const qtd = 12
+// const texto = 'Carrinho (qtd)'
+// console.log(texto)
+
+// const qtd = 12
+// const texto = 'Carrinho (${qtd})'
+// console.log(texto)
